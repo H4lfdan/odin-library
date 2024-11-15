3283253d1911119b83f66ce01cfbafa5b530e755
H4lfdan <danrobnett@gmail.com>
2024-11-15 16:01:17 -0600

    Collapse displayLibrary functionality into addBook method.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function Book(author, title, pageCount, haveRead) {
 }
 
 function addBook() {
+    const table = document.getElementById("display");
     const author = document.getElementById("author").value;
     const title = document.getElementById("title").value;
     const pageCount = document.getElementById("page_count").value;
@@ -16,6 +17,23 @@ function addBook() {
     const book = new Book(author, title, pageCount, haveRead);
     library.push(book);
     console.log("Author: " + book.author + " | Title: " + book.title + " | Page Count: " + book.pageCount + " | Have Read: " + haveRead);
+
+    let currentRow = table.insertRow(-1);
+        let numCell = currentRow.insertCell(-1);
+        let authorCell = currentRow.insertCell(-1);
+        let titleCell = currentRow.insertCell(-1);
+        let pageCell = currentRow.insertCell(-1);
+        let readCell = currentRow.insertCell(-1);
+
+        numCell.innerHTML = `Book ${library.length}:`;
+        authorCell.innerHTML = author;
+        titleCell.innerHTML = title;
+        pageCell.innerHTML = pageCount;
+        if (haveRead) {
+            readCell.innerHTML = "yes";
+        } else if (!haveRead) {
+            readCell.innerHTML = "no";
+        }
 }
 
 function testButton() {
@@ -27,29 +45,29 @@ function testButton() {
 function displayLibrary() {
     const table = document.getElementById("display");
 
-    for(let i = 1, row; row = table.rows[i]; i) {
-        if (table.rows[i].id != "heading") {
-            table.deleteRow(i);
-        }
-    }
+    // for(let i = 1, row; row = table.rows[i]; i) {
+    //     if (table.rows[i].id != "heading") {
+    //         table.deleteRow(i);
+    //     }
+    // }
 
     library.forEach((book, index) => {
-        let currentRow = table.insertRow(index + 1);
-        let numCell = currentRow.insertCell(-1);
-        let authorCell = currentRow.insertCell(-1);
-        let titleCell = currentRow.insertCell(-1);
-        let pageCell = currentRow.insertCell(-1);
-        let readCell = currentRow.insertCell(-1);
+        // let currentRow = table.insertRow(index + 1);
+        // let numCell = currentRow.insertCell(-1);
+        // let authorCell = currentRow.insertCell(-1);
+        // let titleCell = currentRow.insertCell(-1);
+        // let pageCell = currentRow.insertCell(-1);
+        // let readCell = currentRow.insertCell(-1);
 
-        numCell.innerHTML = `Book ${index + 1}:`;
-        authorCell.innerHTML = book.author;
-        titleCell.innerHTML = book.title;
-        pageCell.innerHTML = book.pageCount;
-        if (book.haveRead) {
-            readCell.innerHTML = "yes";
-        } else if (!book.haveRead) {
-            readCell.innerHTML = "no";
-        }
+        // numCell.innerHTML = `Book ${index + 1}:`;
+        // authorCell.innerHTML = book.author;
+        // titleCell.innerHTML = book.title;
+        // pageCell.innerHTML = book.pageCount;
+        // if (book.haveRead) {
+        //     readCell.innerHTML = "yes";
+        // } else if (!book.haveRead) {
+        //     readCell.innerHTML = "no";
+        // }
 
         console.log(`Book ${index + 1}:`);
         console.log(`Author: ${book.author}`);
