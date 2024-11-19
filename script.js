c236730f3fd2d4b1031e22e1b37a707fa594c168
H4lfdan <danrobnett@gmail.com>
2024-11-18 20:10:10 -0600

    Set readCells to display checkboxes instead of text. Checkbox will spawn checked if haveRead is true.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let library = [];
+const table = document.getElementById("display");
 
 function Book(bookNum, author, title, pageCount, haveRead) {
     this.bookNum = bookNum;
@@ -9,7 +10,6 @@ function Book(bookNum, author, title, pageCount, haveRead) {
 }
 
 function addBook() {
-    const table = document.getElementById("display");
     let bookNum = library.length + 1;
     const author = document.getElementById("author").value;
     const title = document.getElementById("title").value;
@@ -32,48 +32,33 @@ function addBook() {
         authorCell.innerHTML = author;
         titleCell.innerHTML = title;
         pageCell.innerHTML = pageCount;
+        readCell.innerHTML = `<input type="checkbox" id="have_read${bookNum}">`;
+        let readCheck = document.getElementById(`have_read${bookNum}`);
         if (haveRead) {
-            readCell.innerHTML = "yes";
+            readCheck.checked = true;
         } else if (!haveRead) {
-            readCell.innerHTML = "no";
+            readCheck.checked = false;
         }
-        remCell.innerHTML = `<button onclick="testButton(${bookNum})">Remove Book ${bookNum}</button>`;
+        remCell.innerHTML = `<button onclick="testButton(${bookNum})">Remove</button>`;
 }
 
 function testButton(remNum) {
-    const table = document.getElementById("display");
-    
     library.splice(remNum - 1, 1);
-
     table.deleteRow(remNum);
 
     library.forEach((book, index) => {
         book.bookNum = index + 1;
         let currentRow = table.rows[book.bookNum];
         let numCell = currentRow.cells[0];
-        // let authorCell = currentRow.insertCell(-1);
-        // let titleCell = currentRow.insertCell(-1);
-        // let pageCell = currentRow.insertCell(-1);
-        // let readCell = currentRow.insertCell(-1);
         let remCell = currentRow.cells[5];
-
         numCell.innerHTML = `Book ${book.bookNum}:`;
-        // authorCell.innerHTML = book.author;
-        // titleCell.innerHTML = book.title;
-        // pageCell.innerHTML = book.pageCount;
-        // if (book.haveRead) {
-        //     readCell.innerHTML = "yes";
-        // } else if (!book.haveRead) {
-        //     readCell.innerHTML = "no";
-        // }
-        remCell.innerHTML = `<button onclick="testButton(${book.bookNum})">Remove Book ${book.bookNum}</button>`;
+        remCell.innerHTML = `<button onclick="testButton(${book.bookNum})">Remove</button>`;
     })
 }
 
 function displayLibrary() {
-    const table = document.getElementById("display");
 
-    library.forEach((book, index) => {
+    library.forEach((book) => {
         console.log(`Book ${book.bookNum} Author: ${book.author}`);
     })
 }
